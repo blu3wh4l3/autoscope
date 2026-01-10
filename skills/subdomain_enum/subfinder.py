e49ad4ffd2dc5f629dc3e9e06cc39ca5fec4eab4
anunv <nvanupb@gmail.com>
2026-01-10 18:05:21 +0000

    Added code to write the subdomain outputs to a file

--- skills/subdomain_enum/subfinder.py
+++ skills/subdomain_enum/subfinder.py
@@ -1,9 +1,16 @@
 from rich.console import Console
-# from rich.spinner import Spinner
+import os
+from pathlib import Path
+
 console = Console()
 def run(domain,executor):
+    project_dir = Path(__file__).resolve().parents[2]
+    domains_output = project_dir / 'outputs' / domain / 'subdomains.txt'
+    domains_output.parent.mkdir(exist_ok=True, parents=True)
     command = f"subfinder -d {domain}"
     with console.status("[bold green] Running subfinder..."):
         output = executor.run(command)
+    with open(domains_output, "w",encoding="utf-8") as f:
+        f.write(output)
     for line in output.splitlines():
         print(line)
